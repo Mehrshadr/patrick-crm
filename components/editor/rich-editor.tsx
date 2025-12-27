3113119c8af60987e4184566ce41bf4dc057b5bc
Mehrshad <mehrshadr@me.com>
2025-12-27 17:24:31 -0500

    fix: TipTap setContent API - use options object

--- components/editor/rich-editor.tsx
+++ components/editor/rich-editor.tsx
@@ -80,7 +80,7 @@ export function RichEditor({ content, onChange, placeholder = "Start writing..."
     // Sync content when it changes externally
     useEffect(() => {
         if (editor && content !== editor.getHTML()) {
-            editor.commands.setContent(content, false)
+            editor.commands.setContent(content, { emitUpdate: false })
         }
     }, [content, editor])
 
